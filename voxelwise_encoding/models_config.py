2d17b08e53d32b1d2954dec2b7974263b0c6b920
Itai Peleg <itaipeleg1@mail.tau.ac.il>
2025-03-27 10:15:07 +0200

    according

--- voxelwise_encoding/models_config.py
+++ voxelwise_encoding/models_config.py
@@ -5,7 +5,10 @@ models_config_dict = {
     'social': ['social_nonsocial'],
     'social_plus_llava': ['arousal', 'mentalization', 'speaking', 'social_nonsocial', 'valence', 'llava_face', 'llava_social', 'llava_touch'],
     'llava_features': ['llava_face', 'llava_social', 'llava_touch'],
-    'llava_only_social': ['llava_social_speak_full'], "llava_music":["social_speak_music"],
+    ## This is the correct annotation for the llava social features
+    'llava_only_social': ['llava_social_speak_full'],
+    "c4":["annotation_c4"],
+      "llava_music":["social_speak_music"],
      'llava_only_face': ['llava_face'],
     'llava_only_face_filled': ['llava_face_filled'],
     "leyla_face": ['face'],
